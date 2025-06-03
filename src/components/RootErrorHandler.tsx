35a5306294ab6f828126ec12ea826e843545c318
PatTheHyruler <pmcroofplays@gmail.com>
2025-06-04 00:21:21 +0300

    "Fix" login required error handler

--- src/components/RootErrorHandler.tsx
+++ src/components/RootErrorHandler.tsx
@@ -1,12 +1,16 @@
 import { LoginRequiredError } from '@/auth/authTypes';
-import { useNavigate } from '@solidjs/router';
+import { createEffect } from 'solid-js';
 
-export const RootErrorHandler = (err: Error | undefined) => {
-    const navigate = useNavigate();
+export const RootErrorHandler = (err: Error | unknown | undefined) => {
+    createEffect(() => {
+        if (err instanceof LoginRequiredError) {
+            // navigate(`/login?returnUrl=${encodeURIComponent(window.location.href)}`);
+            window.location.href = `/login?returnUrl=${encodeURIComponent(window.location.href)}`; // TODO: Figure out a way to use navigate here
+        }
+        throw err;
+    });
 
-    if (err instanceof LoginRequiredError) {
-        navigate(`/login?returnUrl=${encodeURIComponent(window.location.href)}`);
-    }
-
-    return <div>{err?.toString()}</div>;
+    return <pre>
+        {(err instanceof Error ? err.stack : err?.toString())}
+    </pre>;
 };

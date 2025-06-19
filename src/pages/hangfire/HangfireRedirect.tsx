7f09d6eeb195daa347c6bf6ef2ad964721f05bfe
PatTheHyruler <55945965+PatTheHyruler@users.noreply.github.com>
2025-06-20 01:59:29 +0300

    Hangfire routing fixes (#23)
    * Update hangfire auth url
    
    * Update hangfire dashboard url
    
    * Fix relative hangfire redirects

--- src/pages/hangfire/HangfireRedirect.tsx
+++ src/pages/hangfire/HangfireRedirect.tsx
@@ -5,11 +5,11 @@ import { baseApi } from '@/services/baseApi';
 import { useAuthContext } from '@/auth/AuthContext';
 import { isAdmin } from '@/auth/authUtils';
 
-const hangfireUrlString = `${baseApi.baseUrlWithoutPrefix}/hangfire`;
-const hangfireAuthUrlString = `${baseApi.baseUrlWithoutPrefix}/auth/hangfire`;
+const hangfireUrlString = `${baseApi.baseUrlWithoutPrefix}/hangfire-dashboard`;
+const hangfireAuthUrlString = `${baseApi.baseUrl}/v1/auth/hangfire`;
 
 const getTargetUrl = (returnUrlString: string | undefined) => {
-    const hangfireUrl = new URL(hangfireUrlString);
+    const hangfireUrl = new URL(hangfireUrlString, window.location.origin);
 
     if (returnUrlString) {
         const returnUrl = new URL(returnUrlString);
@@ -57,7 +57,7 @@ const HangfireRedirect = () => {
 
         const targetUrl = getTargetUrl(returnUrlString);
 
-        const hangfireAuthUrl = new URL(hangfireAuthUrlString);
+        const hangfireAuthUrl = new URL(hangfireAuthUrlString, window.location.origin);
         const appAuthUrl = new URL(window.location.href);
         appAuthUrl.pathname = '/hangfire/redirect';
         appAuthUrl.search = '';

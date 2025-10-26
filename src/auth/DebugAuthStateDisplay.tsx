fdc16d6d2f761ac1e9c98fe5d25b8515758f0812
PatTheHyruler <55945965+PatTheHyruler@users.noreply.github.com>
2025-10-26 09:07:36 +0200

    Auth debug display improvements (#40)
    * Clean up event listeners
    
    * Clamp auth display value to viewport bounds

--- src/auth/DebugAuthStateDisplay.tsx
+++ src/auth/DebugAuthStateDisplay.tsx
@@ -1,6 +1,25 @@
-import { createEffect, createSignal, Show, useContext } from 'solid-js';
+import { createEffect, createMemo, createSignal, onCleanup, Show, useContext } from 'solid-js';
 import AuthContext from './AuthContext';
 import styles from './DebugAuthStateDisplay.module.css';
+
+interface Position {
+    x: number;
+    y: number;
+}
+
+function clamp(value: number, min: number, max: number): number {
+    return Math.max(Math.min(value, max), min);
+}
+
+function getClampedPosition(position: Position, element: HTMLElement | undefined, rootElement: Element): Position {
+    const defaultWidth = 210;
+    const defaultHeight = 70;
+
+    return {
+        x: clamp(position.x, 0, rootElement.clientWidth - (element?.clientWidth ?? defaultWidth)),
+        y: clamp(position.y, 0, rootElement.clientHeight - (element?.clientHeight ?? defaultHeight)),
+    };
+}
 
 const DebugAuthStateDisplay = () => {
     const authContext = useContext(AuthContext);
@@ -20,6 +39,12 @@ const DebugAuthStateDisplay = () => {
         }
     }
 
+    let debugElement!: HTMLDivElement;
+
+    function getClampedPositionWrapper(position: Position) {
+        return getClampedPosition(position, debugElement, document.documentElement);
+    }
+
     const [state, setState] = createSignal(initialState);
     createEffect(() => {
         localStorage.setItem(debugAuthStateKey, JSON.stringify(state()));
@@ -28,8 +53,9 @@ const DebugAuthStateDisplay = () => {
     const [position, setPosition] = createSignal({
         x: initialState.x,
         y: initialState.y,
-    });
+    } satisfies Position);
     const [isPressed, setIsPressed] = createSignal(false);
+
     const onMove = (e: MouseEvent) => {
         if (isPressed()) {
             setPosition((p) => ({
@@ -39,23 +65,45 @@ const DebugAuthStateDisplay = () => {
         }
     };
 
-    window.addEventListener('mouseup', () => {
+    const onMouseUp = () => {
         const wasPressed = isPressed();
         setIsPressed(false);
         if (wasPressed) {
             setState((v) => ({ ...v, x: position().x, y: position().y }));
         }
+    };
+
+    window.addEventListener('mouseup', onMouseUp);
+    window.addEventListener('mousemove', onMove);
+
+    const resizeObserver = new ResizeObserver((entries) => {
+        if (entries.length <= 0 || !debugElement) {
+            return;
+        }
+
+        const entry = entries[0]!;
+
+        setPosition(p => getClampedPosition(p, debugElement, entry.target));
     });
 
-    window.addEventListener('mousemove', onMove);
+    resizeObserver.observe(document.documentElement);
+
+    onCleanup(() => {
+        window.removeEventListener('mouseup', onMouseUp);
+        window.removeEventListener('mousemove', onMove);
+        resizeObserver.unobserve(document.documentElement);
+    });
+
+    const clampedPosition = createMemo(() => getClampedPositionWrapper(position()));
 
     return (
         <>
             <div
                 class={styles.floating}
                 style={{
-                    transform: `translate(${position().x}px, ${position().y}px)`,
+                    transform: `translate(${clampedPosition().x}px, ${clampedPosition().y}px)`,
                 }}
+                ref={debugElement}
             >
                 <button
                     onMouseDown={(e) => {

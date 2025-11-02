6a708f9e3c784b0d3cdde8786cf6e46d82d939df
PatTheHyruler <pmcroofplays@gmail.com>
2025-11-03 00:52:31 +0200

    Remove pulling job stats

--- src/services/jobsApi.ts
+++
@@ -1,9 +0,0 @@
-import { baseApi } from './baseApi';
-
-const getJobStats = async () => {
-    return await baseApi.axios.get<object>('/v1/jobs/stats');
-};
-
-export const jobsApi = {
-    getJobStats,
-};
